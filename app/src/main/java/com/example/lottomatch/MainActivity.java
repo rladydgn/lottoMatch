9265c43eb0fb83fc0a84db9a5f2816c3f4eebae8
Yonghoo <dhdgn@naver.com>
2021-02-27 01:13:50 +0900

    v2.0.1 fileread

--- app/src/main/java/com/example/lottomatch/MainActivity.java
+++ app/src/main/java/com/example/lottomatch/MainActivity.java
@@ -2,97 +2,88 @@ package com.example.lottomatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
-import android.widget.TextView;
+
+import org.jsoup.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    //logcat
-    private static final String SQL_TAG = "SQL";
-    
-    Button lotteryButton;
-    TextView lottoText;
+
+    // logcat
+    private static final String TAG_DB = "DB";
 
     SQLiteDatabase database;
-    String DB = "lottoNumber";
+    Button lotteryButton;
+
+    int lottoHistory[] = new int[46];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createDatabase();
+        fileRead();
+
         lotteryButton = findViewById(R.id.lotteryButton);
-        lottoText = findViewById(R.id.lottoText);
+        lotteryButton.setOnClickListener(v -> {
 
-        database = openOrCreateDatabase(DB, MODE_PRIVATE, null);
-
-        createDB();
-        upDateDB();
+        });
     }
 
-    public void onLotteryButtonClicked(View v) {
-        int lotto[] = new int[6];
-        int i = 0;
-        while(true) {
-            boolean duplicate = false;
-            // 0~45
-            int ranNum = (int)(Math.random()*45) + 1;
-            // 중복체크
-            for(int k : lotto) {
-                if(k == ranNum)
-                    duplicate = true;
+    private void fileRead() {
+        File f = new File(getFilesDir(), "history.txt");
+        if(!f.isFile()) {
+            Log.d(TAG_DB, "file not exists");
+            try {
+                FileOutputStream fos = new FileOutputStream(f);
+                fos.write(0);
+                fos.close();
             }
-            Log.d(SQL_TAG, ranNum + " ");
-            if(duplicate == true)
-                continue;
-            lotto[i] = ranNum;
-            i += 1;
-            // 6개 모두 뽑았을 경우 반복 종료
-            if(i == lotto.length)
-                break;
+            catch(IOException e) {
+                Log.d(TAG_DB, e + " IOException");
+            }
         }
-
-        database.execSQL("insert into " + DB
-        + "(first, second, third, fourth, fifth, sixth)"
-        + " values "
-        + "(" + lotto[0] + ", " + lotto[1] + ", " + lotto[2] + ", "
-        + lotto[3] + ", " + lotto[4] + ", " + lotto[5] + ")");
-
-        upDateDB();
-    }
-
-    public void onResetButtonClicked(View v) {
-        database.execSQL("DROP TABLE IF EXISTS " + DB);
-        lottoText.setText("");
-        createDB();
-    }
-
-    void upDateDB() {
-        Cursor cursor = database.rawQuery("select _id, first, second, third, fourth, fifth, sixth from " + DB, null);
-        int recordCount = cursor.getCount();
-        lottoText.setText("");
-        for(int n = 0; n < recordCount; n++) {
-            cursor.moveToNext();
-            for(int k = 1; k <= 6; k++)
-                lottoText.append(cursor.getInt(k) + " ");
-            lottoText.append("\n");
+        else {
+            Log.d(TAG_DB, "file exists");
+            try {
+                FileInputStream fis = new FileInputStream(f);
+                int idx = 0, t;
+                while((t = fis.read()) != -1) {
+                    lottoHistory[idx] = t;
+                }
+            }
+            catch(IOException e) {
+                Log.d(TAG_DB, e + " IOException");
+            }
         }
     }
+    private void createDatabase() {
+        Log.d(TAG_DB, "createDatabase called");
 
-    void createDB() {
-        // create SQLite database
-        database.execSQL("create table if not exists " + DB + "("
-                + "_id integer PRIMARY KEY autoincrement, "
-                + " first integer, "
-                + " second integer, "
-                + " third integer, "
-                + " fourth integer, "
-                + " fifth integer, "
-                + " sixth integer)");
+        database = openOrCreateDatabase("lotto", MODE_PRIVATE, null);
+
+        // 추첨번호 table
+        database.execSQL("create table if not exists lottery" + "("
+        + "first integer, "
+        + "second integer, "
+        + "third integer, "
+        + "fourth integer, "
+        + "fifth integer, "
+        + "sixth integer" + ")");
+
+        // 지금 까지 나온 번호, 회차 table
+        database.execSQL("create table if not exists lottoHistory" + "("
+        + "_id integer PRIMARY KEY autoincrement, "
+        + "num integer)");
     }
 }
